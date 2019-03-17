112ea40cfc31d67e7688406626e4777ab203e72c
Phillip <sxh779@case.edu>
2019-03-17 14:21:15 -0400

    found samsung mobile link

--- AmazonScraper.py
+++ AmazonScraper.py
@@ -24,6 +24,21 @@ soup = BeautifulSoup(driver.page_source, "lxml")
 samsunglink = ""
 for med in soup.find_all("div", {"class":"med", "id": "res", "role": "main"}):
     samsungLink = med.find("a").get("href")
-print(samsungLink)
+#print(samsungLink)
 
 
+#We are now going to the amazon page
+li_list = []
+driver.get(samsungLink)
+soup = BeautifulSoup(driver.page_source, "lxml")
+for header in soup.find_all("div", {"id": "header", "class": "a-row stores-row stores-widget-cf"}):
+    for li in soup.find_all("li"):
+        li_list.append(li)
+
+mobileLinks = []
+for a in li_list[1].find_all("a"):
+    mobileLinks.append(a.get("href"))
+
+theMobileLink = "https://www.amazon.com" + mobileLinks[1]
+print(theMobileLink)
+driver.get(theMobileLink)
